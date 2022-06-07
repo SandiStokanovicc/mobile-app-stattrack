4f2c51dd2d09d7496484314da45f6ab27f35048a
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-06-07 18:17:25 +0200

    Connected mobile app with Web API

--- Project/app/src/main/java/learnprogramming/academy/stattrack/MainActivity.java
+++ Project/app/src/main/java/learnprogramming/academy/stattrack/MainActivity.java
@@ -3,12 +3,14 @@ package learnprogramming.academy.stattrack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -18,6 +20,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,47 +36,73 @@ public class MainActivity extends AppCompatActivity {
 
         summonerNameInput = findViewById(R.id.summoner_name_input);
         serverSpinner = findViewById(R.id.server_spinner);
-        Button buttonSearch = findViewById(R.id.search_player_button);
+        //Button buttonSearch = findViewById(R.id.search_player_button);
         resultsFromApi = findViewById(R.id.results_from_api);
         apiRequestQueue = Volley.newRequestQueue(this); // moguce da ce se ovo morati promijeniti kad se promjeni activity
 
+        /*
         buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 jsonParse();
-
             }
         });
-
+        */
         ArrayAdapter<CharSequence> spinnerAdapter=ArrayAdapter.createFromResource(this, R.array.servers,
                 android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         serverSpinner.setAdapter(spinnerAdapter);
     }
-    private void jsonParse(){
-
-        String url = "https://" + serverSpinner.getSelectedItem().toString() +
-                ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerNameInput.getText().toString();
+    public void funkcija(View view){
+        Toast.makeText(MainActivity.this, "PORUKA", Toast.LENGTH_LONG).show();
+    }
+    public void jsonParse(View view){
+        //ovdje radi
+        //Toast.makeText(MainActivity.this, "PORUKA", Toast.LENGTH_LONG).show();
+        String url = "https://stattrack.me/rest/summonersMobileAPI/" + summonerNameInput.getText().toString() + "/" + serverSpinner.getSelectedItem().toString();
+        //String url = "https://" + serverSpinner.getSelectedItem().toString() +
+                //".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerNameInput.getText().toString();
 
         //if it's a JSON array, we will need a JsonArrayRequest
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject responseJSON) {
-                JSONArray jsonArray = responseJSON.getJSONArray("participants"); //ovo moze ako ima vec podijeljen JSON object into arrays
+                /*
+                JSONArray jsonArray = null; //ovo moze ako ima vec podijeljen JSON object into arrays
+                try {
+                    jsonArray = responseJSON.getJSONArray("participants");
+                    //extract objects from array (lol). Ovo bi moglo za participants. matches, etc,
+                    // ali samo za summoner call nije potrebno
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject participants = jsonArray.getJSONObject(i);
+
+                        String firstName = participants.getString("id");
+                        String secondValue = participants.getString("secondValueIdk");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+*/
 
                 //transform object into array
-                //JSONArray jsonArray = new JSONArray();
-                jsonArray.put(responseJSON);
+                Toast.makeText(MainActivity.this, "PORUKA", Toast.LENGTH_LONG).show();
+                try {
 
-                //extract objects from array (lol). Ovo bi moglo za participants. matches, etc,
-                // ali samo za summoner call nije potrebno
-
-                for(int i = 0; i < jsonArray.length(); i++){
-                    JSONObject participants = jsonArray.getJSONObject(i);
-
-                    String firstName = participants.getString("id");
-                    String secondValue = participants.getString("secondValueIdk");
+                    //Toast toast = Toast.makeText(MainActivity.this, "PORUKA", Toast.LENGTH_LONG);
+                    //String id = responseJSON.getString("id");
+                    String name = responseJSON.getString("name");
+                    Log.d("name", name);
+                    String puuid = responseJSON.getString("puuid");
+                    Log.d("puuid", puuid);
+                    resultsFromApi.setText(name + ", " + puuid + "\n\n");
+                    //resultsFromApi.append(name + ", " + puuid + "\n\n");
+                } catch (JSONException e) {
+                    Log.e("ERROR", "onResponse: ERROR");
+                    e.printStackTrace();
                 }
+                //JSONArray jsonArray = new JSONArray();
+                //jsonArray.put(responseJSON);
+                //String id = jsonArray.getString("id");
             }
         }, new Response.ErrorListener() {
             @Override
@@ -81,17 +110,7 @@ public class MainActivity extends AppCompatActivity {
                 error.printStackTrace();
             }
         });
-        /*
-        JsonObjectRequest request = new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
+        apiRequestQueue.add(request);
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        })*/
     }
 }
